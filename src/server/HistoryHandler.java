13eafaee3e62e9bac3c395e29df2366c3f09a07a
Igor <igorek.kopilov@yandex.ru>
2025-02-02 14:57:02 +0300

    Исправить тип возвращаемого значения у обработчиков истории и приоритетов задач на JSON, добавить для этих обработчиков тесты

--- src/server/HistoryHandler.java
+++ src/server/HistoryHandler.java
@@ -1,13 +1,21 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class HistoryHandler extends BaseHttpHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        sendText(httpExchange, HttpTaskServer.taskManager.getHistory().toString());
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        gsonBuilder.registerTypeAdapter(Duration.class, new DurationAdapter());
+        Gson gson = gsonBuilder.create();
+        sendText(httpExchange, gson.toJson(HttpTaskServer.taskManager.getHistory()));
     }
 }
